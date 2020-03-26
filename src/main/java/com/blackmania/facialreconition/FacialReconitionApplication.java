a27cda38c80a3a42bae1fb01389d88bf654952fa
bontib <tfgcguys@gmail.com>
2020-03-26 10:58:48 +0100

    'init'

---
+++ src/main/java/com/blackmania/facialreconition/FacialReconitionApplication.java
@@ -0,0 +1,13 @@
+package com.blackmania.facialreconition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacialReconitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacialReconitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blackmania/facialreconition/FacialReconitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackmania.facialreconition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacialReconitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
