497eefa241047adbf18d80d51ece9b0fc4e084fc
Bon Moolenburgh <40125958+bontib@users.noreply.github.com>
2020-05-14 10:26:35 +0200

    library loaded

--- src/main/java/com/blackmania/facialreconition/FacialReconitionApplication.java
+++ src/main/java/com/blackmania/facialreconition/FacialReconitionApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FacialReconitionApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FacialReconitionApplication.class, args);
     }
