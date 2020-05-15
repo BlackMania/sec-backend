4c30d27883a5e27e1348df1992571fdc86f31890
Bon Moolenburgh <40125958+bontib@users.noreply.github.com>
2020-05-15 10:06:33 +0200

    update

--- src/main/java/com/blackmania/facialreconition/rest/controllers/LoginController.java
+++ src/main/java/com/blackmania/facialreconition/rest/controllers/LoginController.java
@@ -1,9 +1,17 @@
 package com.blackmania.facialreconition.rest.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class LoginController {
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<?> register(){
+        return null;
+    }
+
 }
---
+++ src/main/java/com/blackmania/facialreconition/rest/dto/RegisterDTO.java
@@ -0,0 +1,9 @@
+package com.blackmania.facialreconition.rest.dto;
+
+import java.io.Serializable;
+
+public class RegisterDTO implements Serializable {
+
+
+
+}
